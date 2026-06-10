3e2fc8493933252c02431e209fcaf6b246c0b35e
Rpaz17 <rebeca.paz@unitec.edu>
2026-06-10 17:18:19 -0600

    Cambios en clase 06102026
    --program <ruta del archivo binario del programa en lenguaje de maquina?>
    --font <ruta del archivo MIF con el font>
    --data (Opcional) <ruta de un archivo binario para inicializar el data memory>
    
    se añaden los tres tipos de archivos que debe permitir el sistema, mas el de data memory que es opcional

--- Mips32Soc/CliArgs/CliArgs.cpp
+++ Mips32Soc/CliArgs/CliArgs.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <format>
+#include <cstring>
 #include "CliArgs.hpp"
+
+//--program <ruta del archivo binario del programa en lenguaje de maquina?>
+//--font <ruta del archivo MIF con el font>
+//--data (Opcional) <ruta de un archivo binario para inicializar el data memory>
 
 void CliArgs::printUsage()
 {
@@ -9,27 +14,70 @@ void CliArgs::printUsage()
         << "\n"
         << "Options:\n"
         << "  --file <csv file path>          (Required) Specify the CSV file to work with.\n"
+        << "  --program <path to binary file>     (Required) Specify the binary file of the program in machine language.\n"
+        << "  --font <path to MIF file>           (Required) Specify the MIF file with the font.\n"
+        << "  --data <path to binary file>        (Optional) Specify a binary file to initialize the data memory.\n"
         << "\n";
 }
 
 std::optional<std::string> CliArgs::parse()
 {
-    for (int i = 0; i < argc_; i++) {
-        std::string cmd = argv_[i];
+    for (int i = 0; i < argc_; i++)
+    {
 
-        if (cmd == "--file") {
-            if (i + 1 == argc_) {
+        if (strcmp(argv_[i], "--file") == 0)
+        {
+            if (i + 1 == argc_)
+            {
                 return std::string("Option '--file' requires a file path");
             }
             filepath_ = argv_[i + 1];
             i++;
-        } else {
-            return std::format("Invalid option '{}'", cmd);
+        }
+        else if (strcmp(argv_[i], "--program") == 0)
+        {
+            if (i + 1 == argc_)
+            {
+                return std::string("Option '--program' requires a file path");
+            }
+            machineCode_ = argv_[i + 1];
+            i++;
+        }
+        else if (strcmp(argv_[i], "--font") == 0)
+        {
+            if (i + 1 == argc_)
+            {
+                return std::string("Option '--font' requires a file path");
+            }
+            fontpath_ = argv_[i + 1];
+            i++;
+        }
+        else if (strcmp(argv_[i], "--data") == 0)
+        {
+            if (i + 1 == argc_)
+            {
+                return std::string("Option '--data' requires a file path");
+            }
+            dataFilePath_ = argv_[i + 1];
+            i++;
+        }
+        else
+        {
+            return std::format("Unknown option '{}'", argv_[i]);
         }
     }
 
-    if (filepath_.empty()) {
+    if (filepath_.empty())
+    {
         return std::string("Option '--file' is required");
+    }
+    else if (machineCode_.empty())
+    {
+        return std::string("Option '--program' is required");
+    }
+    else if (fontpath_.empty())
+    {
+        return std::string("Option '--font' is required");
     }
 
     return std::nullopt;
--- Mips32Soc/CliArgs/CliArgs.hpp
+++ Mips32Soc/CliArgs/CliArgs.hpp
@@ -7,19 +7,40 @@ class CliArgs
 {
 public:
     CliArgs(int argc, const char *argv[])
-      : argc_(argc - 1), argv_(argv + 1), program(argv[0])
-    {}
+        : argc_(argc - 1), argv_(argv + 1), program(argv[0])
+    {
+    }
 
     std::string filePath() const
-    { return filepath_; }
+    {
+        return filepath_;
+    }
+
+    std::string machineCode() const
+    {
+        return machineCode_;
+    }
+
+    std::string fontPath() const
+    {
+        return fontpath_;
+    }
+
+    std::optional<std::string> dataFilePath() const
+    {
+        return dataFilePath_;
+    }
 
     void printUsage();
 
     [[nodiscard]] std::optional<std::string> parse();
-    
+
 private:
     int argc_;
     const char **argv_;
     std::string program{};
     std::string filepath_{};
+    std::string fontpath_{};
+    std::string machineCode_{};
+    std::optional<std::string> dataFilePath_{};
 };
